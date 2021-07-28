f756665b5a07e7eef2d4f2fa04e6113068129200
Luiz Felipe Sampaio da Fonseca <luizffons@hotmail.com>
2021-07-28 18:49:50 -0300

    testimonials section added testimonial carousel created script updated to more than 1 carousel

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     //#carouselOurTem
     
-    $(".owl-carousel").owlCarousel({
+    $(".owl-one").owlCarousel({
         items: 4,
         loop: true,
         dots: false,
@@ -27,4 +27,26 @@ $(document).ready(function(){
     //#carouselTestimonials
     //
     
+    $(".owl-two").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        responsive: {
+            0: {
+                items:1,
+            },
+            485 : {
+                items: 1,
+            },
+            728 : {
+                items: 1,
+            },
+            960 : {
+                items: 1,
+            },
+            1200 : {
+                items: 1,
+            },
+        },
+    });
 })
