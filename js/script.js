9bbbaf3e4bcf8be8c926d5eca70fc4d3675abcb9
Luiz Felipe Sampaio da Fonseca <luizffons@hotmail.com>
2021-07-29 12:46:42 -0300

    Financial carousel added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
+    //#carouselTestimonials
+    //
+
+    $(".owl-three").owlCarousel({
+        items: 1,
+        loop: false,
+        dots: false,
+        nav: false,
+        responsive: {
+            0: {
+                items:1,
+            },
+            485 : {
+                items: 1,
+            },
+            728 : {
+                items: 1,
+            },
+            960 : {
+                items: 1,
+            },
+            1200 : {
+                items: 1,
+            },
+        },
+    });
     //#carouselOurTem
     
     $(".owl-one").owlCarousel({
